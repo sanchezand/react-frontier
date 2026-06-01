26f170f1eda2f91f46277264bed4db727d73beb5
Andrés Sánchez <sanchezand97@icloud.com>
2026-06-01 12:48:15 -0600

    Build fixes, removed react-scripts temp

--- rollup.config.js
+++ rollup.config.js
@@ -7,60 +7,72 @@ const sourceMaps = require('rollup-plugin-sourcemaps');
 const { terser } = require('rollup-plugin-terser');
 const typescript = require('rollup-plugin-typescript2');
 const pkg = require('./package.json');
+const copy = require('rollup-plugin-copy');
+const dts = require('rollup-plugin-dts').default;
 
-module.exports = {
-	input: `src/components.ts`,
-	output: [
-		{
-			file: pkg.main,
-			format: 'cjs',
-			sourcemap: true,
-			plugins: [terser()],
-			exports: 'auto',
-		},
-		{
-			file: pkg.module,
-			format: 'es',
-			sourcemap: true,
-			plugins: [terser()],
-			exports: 'auto',
-		},
-		{
-			file: 'dist/index.js',
-			format: 'cjs',
-			sourcemap: true,
-			exports: 'auto',
-		},
-	],
-	watch: {
-		include: 'src/**',
-	},
-	plugins: [
-		external(),
-		postcss({
-			modules: true,
-		}),
-		// Allow json resolution
-		json(),
-		// Compile TypeScript files
-		typescript({
-			useTsconfigDeclarationDir: true,
-			exclude: ['**/__tests__/**', '*.spec.*', '*.test.*'],
-			clean: true,
-			tsconfigOverride: {
-				compilerOptions: {
-					module: "ESNext",
-					moduleResolution: "Bundler",
-				}
+module.exports = [
+	{
+		input: `src/components.ts`,
+		output: [
+			{
+				file: pkg.main,
+				format: 'cjs',
+				sourcemap: true,
+				plugins: [terser()],
+				exports: 'auto',
 			},
-		}),
-		// Allow node_modules resolution, so you can use 'external' to control
-		// which external modules to include in the bundle
-		// https://github.com/rollup/rollup-plugin-node-resolve#usage
-		resolve(),
-		// Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-		commonjs(),
-		// Resolve source maps to the original source
-		sourceMaps(),
-	],
-};
+			{
+				file: pkg.module,
+				format: 'es',
+				sourcemap: true,
+				plugins: [terser()],
+				exports: 'auto',
+			},
+		],
+		watch: {
+			include: 'src/**',
+		},
+		plugins: [
+			copy({
+				targets: [
+					{ src: 'src/locale', dest: 'dist/' },
+					{ src: 'src/style/icons', dest: 'dist/style' },
+				]
+			}),
+			external(),
+			postcss({
+				modules: true,
+			}),
+			json(),
+			typescript({
+				clean: true,
+				declaration: true,
+				useTsconfigDeclarationDir: true,
+				extensions: [".ts", ".tsx"],
+				exclude: ['**/__tests__/**', '*.spec.*', '*.test.*'],
+				clean: true,
+				tsconfigOverride: {
+					compilerOptions: {
+						module: "ESNext",
+						moduleResolution: "node",
+						jsx: 'react-jsx',
+					}
+				},
+			}),
+			resolve(),
+			commonjs(),
+			sourceMaps(),
+		],
+	},
+	{
+		input: "src/components.ts",
+		output: [{
+			file: pkg.types, format: 'es'
+		}],
+		plugins: [
+			external(),
+			postcss({ modules: true }),
+			dts(),
+		]
+	}
+];
